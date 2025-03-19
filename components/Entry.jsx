9d11118b12db7d3704b9dc7b1b2dd342dde2e01e
Yasin Cakal <whoisyasin@gmail.com>
2025-03-18 20:17:08 -0700

    Add files via upload

---
+++ components/Entry.jsx
@@ -0,0 +1,26 @@
+export default function Entry({ img, country, googleMapsLink, title, dates, text }) {
+    return (
+        <article className="journal-entry">
+            <div className="main-image-container">
+                <img 
+                    className="main-image"
+                    src={img.src}
+                    alt={img.alt}
+                />
+            </div>
+            <div className="info-container">
+                <img 
+                    className="marker"
+                    src="../images/marker.png" 
+                    alt="map marker icon"
+                />
+                <span className="country">{country}</span>
+                <a href={googleMapsLink} target="_blank">View on Google Maps</a>
+                <h2 className="entry-title">{title}</h2>
+                <p className="trip-dates">{dates}</p>
+                <p className="entry-text">{text}</p>
+            </div>
+            
+        </article>
+    )
+}
